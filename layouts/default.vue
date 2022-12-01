531a24a449047a9948ab6d97ada076ed3a89eb63
MikhailTo <toshkin.mikhail@yandex.ru>
2022-12-01 22:00:29 +0300

    build(table): add some inputs and data

--- layouts/default.vue
+++ layouts/default.vue
@@ -78,8 +78,8 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          title: 'PermitTable',
+          to: '/PermitTable',
         },
       ],
       miniVariant: false,
